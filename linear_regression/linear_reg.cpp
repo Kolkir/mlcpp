8660a0b0136d46494bf06abfce6f6fb57d913ea5
Kirill Kolodiazhnyi <kolodyazhniy.kirill@gmail.com>
2018-03-20 12:19:41 +0200

    Linear regression finished

--- linear_regression/linear_reg.cpp
+++ linear_regression/linear_reg.cpp
@@ -5,6 +5,9 @@
 #include <csv.h>
 #include <plot.h>
 #include <xtensor/xadapt.hpp>
+#include <xtensor/xarray.hpp>
+#include <xtensor/xbuilder.hpp>
+#include <xtensor/xeval.hpp>
 #include <xtensor/xio.hpp>
 
 // regular includes
@@ -14,7 +17,97 @@
 #include "../utils.h"
 
 // Namespace and type aliases
+using namespace std::literals::string_literals;
 namespace fs = std::experimental::filesystem;
+
+template <typename S, typename T>
+void PrintShape(const S& name, const T& shape) {
+  std::cout << name << " {";
+  for (size_t i = 0; i < shape.size(); ++i) {
+    std::cout << shape[i];
+    if (i < shape.size() - 1)
+      std::cout << ", ";
+  }
+  std::cout << "}\n";
+  std::cout.flush();
+}
+
+template <typename Vb, typename Vx>
+auto predict(const Vb& b, const Vx& x) {
+  return xt::sum(x * b, {1});
+}
+
+template <typename V>
+auto minmax_scale(const V& v) {
+  if (v.shape().size() == 1) {
+    auto minmax = xt::minmax(v)();
+    xt::xarray<typename V::value_type> vs =
+        (v - minmax[0]) / (minmax[1] - minmax[0]);
+    return vs;
+  } else if (v.shape().size() == 2) {
+    auto w = v.shape()[1];
+    xt::xarray<typename V::value_type> vs =
+        xt::zeros<typename V::value_type>(v.shape());
+    for (decltype(w) j = 0; j < w; ++j) {
+      auto vc = xt::view(v, xt::all(), j);
+      auto vsc = xt::view(vs, xt::all(), j);
+      auto minmax = xt::minmax(vc)();
+      vsc = (vc - minmax[0]) / (minmax[1] - minmax[0]);
+    }
+    return vs;
+  } else {
+    throw std::logic_error("Minmax scale unsupported dimensions");
+  }
+}
+
+template <typename Vx, typename Vy>
+auto gd(const Vx& x, const Vy& y) {
+  size_t n_epochs = 1000;
+  float lr = 0.001;
+  auto n = x.shape()[0];
+  auto w = x.shape()[1];
+
+  xt::xarray<typename Vx::value_type> b =
+      xt::zeros<typename Vx::value_type>({w});
+  xt::xarray<typename Vx::value_type> grad =
+      xt::zeros<typename Vx::value_type>({w});
+
+  for (size_t i = 0; i < n_epochs; ++i) {
+    auto yhat = predict(b, x);
+    auto error = yhat - y;
+    for (size_t j = 0; j < w; ++j) {
+      auto xc = xt::view(x, xt::all(), j);
+      grad[j] = (xt::sum(xc * error) / n)();
+    }
+    b = b - lr * grad;
+    auto cost = (xt::sum(xt::pow(error, 2)) / n)();
+    std::cout << "Iteration : " << i << " Cost = " << cost << " b0 = " << b[0]
+              << " b1 = " << b[1] << std::endl;
+  }
+  return b;
+}
+
+template <typename Vx, typename Vy>
+auto straight_line_model(const Vx& data_x, const Vy& data_y) {
+  // minmax scaling
+  auto y = minmax_scale(data_y);
+
+  xt::xarray<float> x = xt::zeros<float>(data_x.shape());
+  x = data_x;
+  auto xv = xt::view(x, xt::all(), xt::range(1, xt::placeholders::_));
+  xv = minmax_scale(xv);
+
+  // learn line parameters with Gradient Descent
+  auto b = gd(x, y);
+
+  // model with line
+  xt::xarray<float> line_values = predict(b, x);
+
+  // restore scaling for predicted line values
+  auto y_minmax = xt::minmax(data_y)();
+  line_values = line_values * (y_minmax[1] - y_minmax[0]) + y_minmax[0];
+  return line_values;
+}
 
 int main() {
   // Download the data
@@ -33,21 +126,36 @@ int main() {
   io::CSVReader<cols, io::trim_chars<' '>, io::no_quote_escape<'\t'>> data_tsv(
       data_path);
 
-  std::vector<float> x;
-  std::vector<float> y;
+  std::vector<float> raw_data_x;
+  std::vector<float> raw_data_y;
+
   bool done = false;
   do {
     try {
-      float x_v = 0, y_v = 0;
-      done = !data_tsv.read_row(x_v, y_v);
-      x.push_back(x_v);
-      y.push_back(y_v);
+      float x = 0, y = 0;
+      done = !data_tsv.read_row(x, y);
+      raw_data_x.push_back(1);  // additional x for simpler vectorization
+      raw_data_x.push_back(x);
+      raw_data_y.push_back(y);
     } catch (const io::error::no_digit& err) {
+      // ignore bad formated samples
       std::cout << err.what() << std::endl;
     }
   } while (!done);
 
-  // plot data we read
+  // map data to the tensor
+  size_t rows = raw_data_x.size() / 2;
+  auto shape_x = std::vector<size_t>{rows, size_t{2}};
+  auto data_x = xt::adapt(raw_data_x, shape_x);
+  PrintShape("X"s, data_x.shape());
+
+  auto shape_y = std::vector<size_t>{rows};
+  auto data_y = xt::adapt(raw_data_y, shape_y);
+  PrintShape("Y"s, data_y.shape());
+
+  xt::xarray<float> line_values = straight_line_model(data_x, data_y);
+
+  // plot the data we read
   plotcpp::Plot plt(true);
   plt.SetTerminal("qt");
   plt.SetTitle("Web traffic over the last month");
@@ -56,7 +164,7 @@ int main() {
   plt.SetAutoscale();
   plt.GnuplotCommand("set grid");
 
-  auto minmax = std::minmax_element(x.begin(), x.end());
+  auto minmax = std::minmax_element(data_x.begin(), data_x.end());
   auto diff = *minmax.second - *minmax.first;
   auto tic_size = 7 * 24;
   auto time_tics = diff / tic_size;
@@ -71,17 +179,12 @@ int main() {
   xtics_labels << ")";
   plt.GnuplotCommand(xtics_labels.str());
 
-  plt.Draw2D(plotcpp::Points(x.begin(), x.end(), y.begin(), "points"));
+  auto x_coord = xt::view(data_x, xt::all(), 1);
+  plt.Draw2D(
+      plotcpp::Points(x_coord.begin(), x_coord.end(), data_y.begin(), "points"),
+      plotcpp::Lines(x_coord.begin(), x_coord.end(), line_values.begin(),
+                     "line approx"));
   plt.Flush();
-
-  // fit polynom to our data
-  std::vector<int> shape = {static_cast<int>(x.size()), 1};
-  auto data_x = xt::adapt(x, shape);
-  auto data_y = xt::adapt(y, shape);
-  auto data = xt::stack(xt::xtuple(data_x, data_y), 1);
-  std::cout << data.shape()[0] << " " << data.shape()[1];
-
-  // xt::zeros<float> data{{3, 4}};
 
   return 0;
 }
