9f3636ec1fa460085aa39cd21c7b4c385bd3cf0b
Kirill Kolodiazhnyi <kolodyazhniy.kirill@gmail.com>
2018-11-24 23:56:25 +0200

    Add sample how to export weight from Python

--- mask_rcnn_pytorch/stateloader.h
+++ mask_rcnn_pytorch/stateloader.h
@@ -4,6 +4,20 @@
 #include <torch/torch.h>
 #include <vector>
 
+/* Correspondig Python export code
+ * raw_state_dict = {}
+ * for k, v in model.state_dict().items():
+ *     if isinstance(v, torch.Tensor):
+ *         raw_state_dict[k] = (list(v.size()), v.numpy().tolist())
+ *         break
+ *     else:
+ *         print("State parameter type error : {}".format(k))
+ *         exit(-1)
+ *
+ * with open('mask_rcnn_coco.json', 'w') as outfile:
+ *     json.dump(raw_state_dict, outfile)
+ */
+
 torch::OrderedDict<std::string, torch::Tensor> LoadStateDict(
     const std::string& file_name);
 
